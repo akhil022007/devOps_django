4b9a6453635092310231bca09b1611c8040d92b0
“akhil022006” <akhilragala@gmail.com>
2025-07-09 18:24:29 +0530

    Complete project refresh: All files updated for robust CI/CD

--- myproject/urls.py
+++ myproject/urls.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
-from django.urls import path, include # Import include
+from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('myapp.urls')), # Include your app's URLs here
+
 ]
 
